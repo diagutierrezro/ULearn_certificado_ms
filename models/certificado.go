0156f25a9af59614afe9d0180be5c4b95a70b3a0
Diego Gutierrez <diegoa970411@gmail.com>
2020-04-10 18:22:18 -0500

    first commit

---
+++ models/certificado.go
@@ -0,0 +1,172 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/astaxie/beego/orm"
+)
+
+type Certificado struct {
+	Id        int                `orm:"column(id_certificado);pk;auto"`
+	Texto     string             `orm:"column(texto)"`
+	IdUsuario *DatosUsuarioCurso `orm:"column(id_usuario);rel(fk)"`
+	IdCurso   int                `orm:"column(id_curso)"`
+}
+
+func (t *Certificado) TableName() string {
+	return "certificado"
+}
+
+func init() {
+	orm.RegisterModel(new(Certificado))
+}
+
+// AddCertificado insert a new Certificado into database and returns
+// last inserted Id on success.
+func AddCertificado(m *Certificado) (id int64, err error) {
+	o := orm.NewOrm()
+	v := m.IdUsuario
+	e := o.QueryTable("DatosUsuarioCurso").Filter("Id", m.IdUsuario).RelatedSel().One(v)
+	fmt.Println(e)
+	m.IdCurso = v.IdCurso
+	m.Texto = "El usuario " + v.NombreUsuario + " identificado con C.C " + strconv.Itoa(v.DocumentoUsuario) +
+		" completó satisfactoriamente el curso de " + v.NombreCurso + "el cual es ofrecido por U_Learn. " +
+		"Por lo tanto se le es otorgado el certificado"
+	id, err = o.Insert(m)
+	return
+}
+
+// GetCertificadoById retrieves Certificado by Id. Returns error if
+// Id doesn't exist
+/*func GetCertificadoById(id int) (v *Certificado, err error) {
+	o := orm.NewOrm()
+	v = &Certificado{Id: id}
+	if err = o.Read(v); err == nil {
+		return v, nil
+	}
+	return nil, err
+}*/
+
+func GetCertificadoById(id int) (v *Certificado, err error) {
+	o := orm.NewOrm()
+	v = &Certificado{}
+	e := o.QueryTable("certificado").Filter("Id", id).RelatedSel().One(v)
+	if e == nil {
+		return v, nil
+	} else {
+		fmt.Println(e)
+	}
+	return nil, err
+}
+
+// GetAllCertificado retrieves all Certificado matches certain condition. Returns empty list if
+// no records exist
+func GetAllCertificado(query map[string]string, fields []string, sortby []string, order []string,
+	offset int64, limit int64) (ml []interface{}, err error) {
+	o := orm.NewOrm()
+	qs := o.QueryTable(new(Certificado)).RelatedSel()
+	// query k=v
+	for k, v := range query {
+		// rewrite dot-notation to Object__Attribute
+		k = strings.Replace(k, ".", "__", -1)
+		if strings.Contains(k, "isnull") {
+			qs = qs.Filter(k, (v == "true" || v == "1"))
+		} else {
+			qs = qs.Filter(k, v)
+		}
+	}
+	// order by:
+	var sortFields []string
+	if len(sortby) != 0 {
+		if len(sortby) == len(order) {
+			// 1) for each sort field, there is an associated order
+			for i, v := range sortby {
+				orderby := ""
+				if order[i] == "desc" {
+					orderby = "-" + v
+				} else if order[i] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+			qs = qs.OrderBy(sortFields...)
+		} else if len(sortby) != len(order) && len(order) == 1 {
+			// 2) there is exactly one order, all the sorted fields will be sorted by this order
+			for _, v := range sortby {
+				orderby := ""
+				if order[0] == "desc" {
+					orderby = "-" + v
+				} else if order[0] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+		} else if len(sortby) != len(order) && len(order) != 1 {
+			return nil, errors.New("Error: 'sortby', 'order' sizes mismatch or 'order' size is not 1")
+		}
+	} else {
+		if len(order) != 0 {
+			return nil, errors.New("Error: unused 'order' fields")
+		}
+	}
+
+	var l []Certificado
+	qs = qs.OrderBy(sortFields...)
+	if _, err = qs.Limit(limit, offset).All(&l, fields...); err == nil {
+		if len(fields) == 0 {
+			for _, v := range l {
+				ml = append(ml, v)
+			}
+		} else {
+			// trim unused fields
+			for _, v := range l {
+				m := make(map[string]interface{})
+				val := reflect.ValueOf(v)
+				for _, fname := range fields {
+					m[fname] = val.FieldByName(fname).Interface()
+				}
+				ml = append(ml, m)
+			}
+		}
+		return ml, nil
+	}
+	return nil, err
+}
+
+// UpdateCertificado updates Certificado by Id and returns error if
+// the record to be updated doesn't exist
+func UpdateCertificadoById(m *Certificado) (err error) {
+	o := orm.NewOrm()
+	v := Certificado{Id: m.Id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Update(m); err == nil {
+			fmt.Println("Number of records updated in database:", num)
+		}
+	}
+	return
+}
+
+// DeleteCertificado deletes Certificado by Id and returns error if
+// the record to be deleted doesn't exist
+func DeleteCertificado(id int) (err error) {
+	o := orm.NewOrm()
+	v := Certificado{Id: id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Delete(&Certificado{Id: id}); err == nil {
+			fmt.Println("Number of records deleted in database:", num)
+		}
+	}
+	return
+}
---
+++ models/datos_usuario_curso.go
@@ -0,0 +1,154 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/astaxie/beego/orm"
+)
+
+type DatosUsuarioCurso struct {
+	Id               int    `orm:"column(id_datos_usuario_curso);pk;auto"`
+	IdUsuario        int    `orm:"column(id_usuario)"`
+	IdCurso          int    `orm:"column(id_curso)"`
+	NombreCurso      string `orm:"column(nombre_curso)"`
+	NombreUsuario    string `orm:"column(nombre_usuario)"`
+	DocumentoUsuario int    `orm:"column(documento_usuario)"`
+}
+
+func (t *DatosUsuarioCurso) TableName() string {
+	return "datos_usuario_curso"
+}
+
+func init() {
+	orm.RegisterModel(new(DatosUsuarioCurso))
+}
+
+// AddDatosUsuarioCurso insert a new DatosUsuarioCurso into database and returns
+// last inserted Id on success.
+func AddDatosUsuarioCurso(m *DatosUsuarioCurso) (id int64, err error) {
+	o := orm.NewOrm()
+	id, err = o.Insert(m)
+	return
+}
+
+// GetDatosUsuarioCursoById retrieves DatosUsuarioCurso by Id. Returns error if
+// Id doesn't exist
+func GetDatosUsuarioCursoById(id int) (v *DatosUsuarioCurso, err error) {
+	o := orm.NewOrm()
+	v = &DatosUsuarioCurso{Id: id}
+	if err = o.Read(v); err == nil {
+		return v, nil
+	}
+	return nil, err
+}
+
+// GetAllDatosUsuarioCurso retrieves all DatosUsuarioCurso matches certain condition. Returns empty list if
+// no records exist
+func GetAllDatosUsuarioCurso(query map[string]string, fields []string, sortby []string, order []string,
+	offset int64, limit int64) (ml []interface{}, err error) {
+	o := orm.NewOrm()
+	qs := o.QueryTable(new(DatosUsuarioCurso))
+	// query k=v
+	for k, v := range query {
+		// rewrite dot-notation to Object__Attribute
+		k = strings.Replace(k, ".", "__", -1)
+		if strings.Contains(k, "isnull") {
+			qs = qs.Filter(k, (v == "true" || v == "1"))
+		} else {
+			qs = qs.Filter(k, v)
+		}
+	}
+	// order by:
+	var sortFields []string
+	if len(sortby) != 0 {
+		if len(sortby) == len(order) {
+			// 1) for each sort field, there is an associated order
+			for i, v := range sortby {
+				orderby := ""
+				if order[i] == "desc" {
+					orderby = "-" + v
+				} else if order[i] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+			qs = qs.OrderBy(sortFields...)
+		} else if len(sortby) != len(order) && len(order) == 1 {
+			// 2) there is exactly one order, all the sorted fields will be sorted by this order
+			for _, v := range sortby {
+				orderby := ""
+				if order[0] == "desc" {
+					orderby = "-" + v
+				} else if order[0] == "asc" {
+					orderby = v
+				} else {
+					return nil, errors.New("Error: Invalid order. Must be either [asc|desc]")
+				}
+				sortFields = append(sortFields, orderby)
+			}
+		} else if len(sortby) != len(order) && len(order) != 1 {
+			return nil, errors.New("Error: 'sortby', 'order' sizes mismatch or 'order' size is not 1")
+		}
+	} else {
+		if len(order) != 0 {
+			return nil, errors.New("Error: unused 'order' fields")
+		}
+	}
+
+	var l []DatosUsuarioCurso
+	qs = qs.OrderBy(sortFields...)
+	if _, err = qs.Limit(limit, offset).All(&l, fields...); err == nil {
+		if len(fields) == 0 {
+			for _, v := range l {
+				ml = append(ml, v)
+			}
+		} else {
+			// trim unused fields
+			for _, v := range l {
+				m := make(map[string]interface{})
+				val := reflect.ValueOf(v)
+				for _, fname := range fields {
+					m[fname] = val.FieldByName(fname).Interface()
+				}
+				ml = append(ml, m)
+			}
+		}
+		return ml, nil
+	}
+	return nil, err
+}
+
+// UpdateDatosUsuarioCurso updates DatosUsuarioCurso by Id and returns error if
+// the record to be updated doesn't exist
+func UpdateDatosUsuarioCursoById(m *DatosUsuarioCurso) (err error) {
+	o := orm.NewOrm()
+	v := DatosUsuarioCurso{Id: m.Id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Update(m); err == nil {
+			fmt.Println("Number of records updated in database:", num)
+		}
+	}
+	return
+}
+
+// DeleteDatosUsuarioCurso deletes DatosUsuarioCurso by Id and returns error if
+// the record to be deleted doesn't exist
+func DeleteDatosUsuarioCurso(id int) (err error) {
+	o := orm.NewOrm()
+	v := DatosUsuarioCurso{Id: id}
+	// ascertain id exists in the database
+	if err = o.Read(&v); err == nil {
+		var num int64
+		if num, err = o.Delete(&DatosUsuarioCurso{Id: id}); err == nil {
+			fmt.Println("Number of records deleted in database:", num)
+		}
+	}
+	return
+}
